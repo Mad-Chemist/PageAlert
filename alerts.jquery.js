5c715efeae11b5b50475bace27e7fe5c95710150
Mad-Chemist <vico@b3wd.com>
2013-12-17 14:44:48 -0500

    Added comments

--- alerts.jquery.js
+++ alerts.jquery.js
@@ -1,7 +1,16 @@
-var alertNumber =0, alertWidth = 300, alertDelay = 4000;
+/* 
+ *
+ * Copyright : 	(C) 2013 Vico Bertogli 
+ * Author	 : 	Vico Bertogli III
+ * Website 	 : 	http://www.b3wd.com
+ * Github  	 : 	https://github.com/Mad-Chemist
+ *
+ */
+
+var alertNumber =0, alertWidth = '300px', alertDelay = 4000;
 $(document).ready(function() {
 	$('body').append('<div id="pageAlerts"></div>');
-	$("<style type='text/css'>#pageAlerts { position:fixed; top:0; width:100%; z-index:9999999999999; pointer-events:none;} #pageAlerts .alert {width: "+alertWidth+"px; margin: 5px auto; color: #FFF; display: block; padding: 10px; text-align: center; font-weight: bold; z-index:99999999999999;} #pageAlerts .alert.good { background:green; } #pageAlerts .alert.bad { background:#E00000;;} .round6 { -webkit-border-radius: 6px; -moz-border-radius: 6px; border-radius: 6px; }</style>").appendTo("head");
+	$("<style type='text/css'>#pageAlerts { position:fixed; top:0; width:100%; z-index:9999999999999; pointer-events:none;} #pageAlerts .alert {width: "+alertWidth+"; margin: 5px auto; color: #FFF; display: block; padding: 10px; text-align: center; font-weight: bold; z-index:99999999999999;} #pageAlerts .alert.good { background:green; } #pageAlerts .alert.bad { background:#E00000;;} .round6 { -webkit-border-radius: 6px; -moz-border-radius: 6px; border-radius: 6px; }</style>").appendTo("head");
 });
 $.fn.pageAlert = function(alert,type,color) {
 	var type 	 = (type == 1) ? "bad" : "good";
@@ -12,10 +21,14 @@ $.fn.pageAlert = function(alert,type,color) {
 };
 
 
+//CUSTOMIZATION
+
+//set alertWidth to the width you want the alerts to be shown at
+//set alertDelay to the delay you want the alerts to linger for
 
 //DEFINITION
 
-// $(WHERE YOU WANT ALERT TO SHOW -> for best results this should be '#pageAlerts').pageAlert('TEXT YOU WANT SHOWN', 1 for red and 0 for green background)
+// $(WHERE YOU WANT ALERT TO SHOW -> for best results this should be '#pageAlerts').pageAlert('TEXT YOU WANT SHOWN', TYPE - > 1 for red and 0 for green background, CUSTOM BACKGROUND COLOR -> must be in hex format);
 
 // USAGE EXAMPLES:
 
