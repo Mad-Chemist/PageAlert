1d541655d740362063588e70e7cdacabd753cf67
Mad-Chemist <vico@b3wd.com>
2014-01-15 00:42:01 -0500

    expanded functionality
    params =
    {
    'url' : 'string',
    'title' : 'string',
    'theme' : 'string',
    'class' : 'string',
    'delay' : 'int'
    
    }

--- alerts.jquery.js
+++ alerts.jquery.js
@@ -7,35 +7,36 @@
  *
  */
 
-var alertNumber =0, alertWidth = '300px', alertDelay = 4000;
+var alertNumber =0, alertWidth = '300px', alertDelay = 4000, alertDefaultTheme = 'teal';
 $(document).ready(function() {
+	$('head').append('<style id="alert-title"></style>');
 	$('body').append('<div id="pageAlerts"></div>');
-	$("<style type='text/css'>#pageAlerts { position:fixed; top:0; width:100%; z-index:9999999999999; pointer-events:none;} #pageAlerts .alert {width: "+alertWidth+"; margin: 5px auto; color: #FFF; display: block; padding: 10px; text-align: center; font-weight: bold; z-index:99999999999999;} #pageAlerts .alert.good { background:green; } #pageAlerts .alert.bad { background:#E00000;;} .round6 { -webkit-border-radius: 6px; -moz-border-radius: 6px; border-radius: 6px; }</style>").appendTo("head");
 });
-$.fn.pageAlert = function(alert,type,color) {
-	var type 	 = (type == 1) ? "bad" : "good";
-	var addColor = (color !== 'undefined') ? "style='background:#"+color+"';" : "";
-	$(this).append('<span class="round6 alert '+type+'" id="alert'+alertNumber+'" '+addColor+'>'+alert+'</span>');
-	$('#alert'+alertNumber).delay(alertDelay).fadeOut(function() { $(this).remove(); });
+function pagealert(message,params) {
+	if (typeof message === 'undefined') return; //if no message, exit
+
+	if (typeof params === 'undefined') var params = {}; //if array doesnt exist, create it
+
+	if (typeof params['type'] !== 'undefined') $('#alert-title').append('#alert'+alertNumber+':before { content: "'+ params['type']+'"; }'); //add left content if requested
+	params['theme'] = (typeof params['theme'] === 'undefined') 	? alertDefaultTheme+'-alert ' : params['delay']+'-alert ';
+	params['class'] = (typeof params['class'] === 'undefined') 	? '' : params['class'];
+	params['delay'] = (typeof params['delay'] === 'undefined') 	? alertDelay : params['delay'];
+	params['url'] 	= (typeof params['url'] === 'undefined') 	? '' : 'href="'+params['url']+'"';
+	
+
+
+	$('#pageAlerts').append('<a id="alert'+alertNumber+'" class="notice '+params['theme']+params['class']+'" '+params['url']+'>'+message+'</a>');
+	$('#alert'+alertNumber).width($('#alert'+alertNumber).outerWidth()).css('display','block');
+	if (params['delay'] !== 0) $('#alert'+alertNumber).delay(params['delay']).fadeOut('slow',function() { $(this).remove(); });
 	alertNumber++;
-};
+}
 
+/*params = [
+{
+	'url' : 'string',
+	'title' : 'string',
+	'theme' : 'string',
+	'class' : 'string',
+	'delay' : 'int'
 
-//CUSTOMIZATION
-
-//set alertWidth to the width you want the alerts to be shown at
-//set alertDelay to the delay you want the alerts to linger for
-
-//DEFINITION
-
-// $(WHERE YOU WANT ALERT TO SHOW -> for best results this should be '#pageAlerts').pageAlert('TEXT YOU WANT SHOWN', TYPE - > 1 for red and 0 for green background, CUSTOM BACKGROUND COLOR -> must be in hex format);
-
-// USAGE EXAMPLES:
-
-// $('#pageAlerts').pageAlert('This is a sample green alert',0); ---> This will add a green alert alert to the alert container previously defined
-
-// $('#pageAlerts').pageAlert('This is a sample red alert',1); ---> This will add a red alert alert to the alert container previously defined
-
-// $('#pageAlerts').pageAlert('This is a sample custom color alert',1, 333333); ---> This will add a dark grey alert alert to the alert container previously defined.  The third paramater requires the values to be in hex format
-
-// $('body').pageAlert('This is a sample red alert',1); ---> This adds the alert to the body, instead of the defined container
+} ;*/
