e830fcce87c23e3387ebe8a6ff4d78abbf5cd531
Vico Bertogli <vico@b3wd.com>
2014-01-15 10:53:06 -0500

    $ changed to jQuery

--- alerts.jquery.js
+++ alerts.jquery.js
@@ -8,16 +8,16 @@
  */
 
 var alertNumber =0, alertWidth = '300px', alertDelay = 4000, alertDefaultTheme = 'teal';
-$(document).ready(function() {
-	$('head').append('<style id="alert-title"></style>');
-	$('body').append('<div id="pageAlerts"></div>');
+jQuery(document).ready(function() {
+	jQuery('head').append('<style id="alert-title"></style>');
+	jQuery('body').append('<div id="pageAlerts"></div>');
 });
 function pagealert(message,params) {
 	if (typeof message === 'undefined') return; //if no message, exit
 
 	if (typeof params === 'undefined') var params = {}; //if array doesnt exist, create it
 
-	if (typeof params['type'] !== 'undefined') $('#alert-title').append('#alert'+alertNumber+':before { content: "'+ params['type']+'"; }'); //add left content if requested
+	if (typeof params['type'] !== 'undefined') jQuery('#alert-title').append('#alert'+alertNumber+':before { content: "'+ params['type']+'"; }'); //add left content if requested
 	params['theme'] = (typeof params['theme'] === 'undefined') 	? alertDefaultTheme+'-alert ' : params['delay']+'-alert ';
 	params['class'] = (typeof params['class'] === 'undefined') 	? '' : params['class'];
 	params['delay'] = (typeof params['delay'] === 'undefined') 	? alertDelay : params['delay'];
@@ -25,16 +25,16 @@ function pagealert(message,params) {
 	
 
 
-	$('#pageAlerts').append('<a id="alert'+alertNumber+'" class="notice '+params['theme']+params['class']+'" '+params['url']+'>'+message+'</a>');
-	$('#alert'+alertNumber).width($('#alert'+alertNumber).outerWidth()).css('display','block');
-	if (params['delay'] !== 0) $('#alert'+alertNumber).delay(params['delay']).fadeOut('slow',function() { $(this).remove(); });
+	jQuery('#pageAlerts').append('<a id="alert'+alertNumber+'" class="notice '+params['theme']+params['class']+'" '+params['url']+'>'+message+'</a>');
+	jQuery('#alert'+alertNumber).width(jQuery('#alert'+alertNumber).outerWidth()).css('display','block');
+	if (params['delay'] !== 0) jQuery('#alert'+alertNumber).delay(params['delay']).fadeOut('slow',function() { jQuery(this).remove(); });
 	alertNumber++;
 }
 
 /*params = [
 {
 	'url' : 'string',
-	'title' : 'string',
+	'type' : 'string',
 	'theme' : 'string',
 	'class' : 'string',
 	'delay' : 'int'
